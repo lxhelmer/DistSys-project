2830b12127cf78cbe018d0f5168cabaef9b215e6
afnan1456 <baig.afnan1456@gmail.com>
2024-12-10 23:15:51 +0500

    synchronization

--- src/node.py
+++ src/node.py
@@ -66,7 +66,7 @@ def read_data(data, client_socket: socket.socket):
     elif data["type"] == "client_pause":
         pass
     elif data["type"] == "client_play":
-        initiate_playback(data["content_id"], data["action"], data["scheduled_time"], node_id=NODE_ID, node_host=NODE_HOST, node_port=NODE_PORT, NODES_LIST=NODES)
+        initiate_playback(data["content_id"], data["action"], data["time_after"], node_id=NODE_ID, node_host=NODE_HOST, node_port=NODE_PORT, NODES_LIST=NODES)
     elif data["type"] == "client_stop":
         pass
     elif data["type"] == "init_playback":
