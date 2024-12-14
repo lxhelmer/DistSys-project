5f38e22fe3e7a63fbbe2bb3d7afdb8a47a199cc8
Helmer Lauronen <helmer.lauronen@gmail.com>
2024-12-15 01:43:20 +0200

    Fix variable typo

--- src/node_3.py
+++ src/node_3.py
@@ -153,7 +153,7 @@ def handle_file_update(data, client_socket):
     for r_file in recv_files:
         if r_file not in FILES:
             print("file missing:",r_file)
-            handle_ask_file("r_file",client_socket)
+            handle_ask_file(r_file, client_socket)
 
 
 def handle_send_file(file_name, client_socket):
@@ -168,7 +168,7 @@ def handle_send_file(file_name, client_socket):
 def handle_ask_file(file_name, client_socket):
     print("Sending request for missing file")
     client_socket.send(json.dumps({"type": "file_request", "HOST": NODE_HOST, "PORT": NODE_PORT, "NODE_ID": NODE_ID, "file_name": file_name}).encode('utf-8'))
-    with open("../data/"+r_file, "wb") as f:
+    with open("../data/"+file_name, "wb") as f:
         while True:
             recv_bytes = client_socket.recv(BUFFER_SIZE)
             if not recv_bytes:
